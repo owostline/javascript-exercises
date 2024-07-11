f4354ea3cfd11e8a318df6a8ff62b5ec2b227f7e
owostline <ostlinecasao@gmail.com>
2024-07-11 23:21:27 +0800

    passed tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    let string = '';
+    if (times < 0){
+        return 'ERROR';
+    }
 
-};
+    for (let i = 0; i < -1; i++) {
+      string += word;
+    }
+    return string;
+ }
+  
+    console.log (repeatString('', 10));
 
 // Do not edit below this line
 module.exports = repeatString;
