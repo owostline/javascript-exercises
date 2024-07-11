40ca434f465b7a5f7d59842401a8387df94139cd
owostline <ostlinecasao@gmail.com>
2024-07-12 00:03:59 +0800

    passed tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    var splitString = word.split("");
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join('');
+    return joinArray;
+    }
 
-};
-
+    reverseString('');
 // Do not edit below this line
 module.exports = reverseString;
