ef3c961ce47179ffcc587409d82c6a5e19c14bd1
owostline <ostlinecasao@gmail.com>
2024-07-12 15:48:40 +0800

    passed tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...valuesToRemove) {
+    return array.filter(function (element) {
+        return !valuesToRemove.includes(element);
+    });
 };
 
+removeFromArray(['hey', 2, 3, 'ho'], '1',3);
 // Do not edit below this line
 module.exports = removeFromArray;
