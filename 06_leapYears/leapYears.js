3526ef2349258dac5ed80c7dc81b763cb0cb5611
owostline <ostlinecasao@gmail.com>
2024-07-13 01:30:20 +0800

    passed tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,25 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let leapYear; 
 
+    //checks if year is divisible by 4
+    if (year % 4 == 0) {
+        leapYear = true;
+    } else {
+        leapYear = false;
+    }
+
+    //checks if year is both divisible by 100 and 400
+    if (leapYear == true && year % 100 == 0) {
+        if (year % 400 == 0) {
+            leapYear = true; 
+        } else {
+            leapYear = false;
+        }
+    }
+       
+    return Boolean(leapYear); 
 };
 
+leapYears(700);
 // Do not edit below this line
 module.exports = leapYears;
