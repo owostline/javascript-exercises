15484a01e32e57137c58e1ed4dfabc18ed99e4c3
owostline <ostlinecasao@gmail.com>
2024-07-13 14:44:24 +0800

    passed tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitGiven) {
+  let celciusConverted;
+  celciusConverted = (fahrenheitGiven - 32) * (5 / 9);
+  return Math.round(celciusConverted * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celciusGiven) {
+  let fahrenheitConverted;
+  fahrenheitConverted = (celciusGiven * 9/5) + 32;
+  return Math.round(fahrenheitConverted * 10) / 10;
 };
+
+convertToCelsius();
+convertToFahrenheit(-10);
 
 // Do not edit below this line
 module.exports = {
